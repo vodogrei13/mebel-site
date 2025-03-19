039f8cac995189486ed81fb9ddbf4520042f584b
Alex Vodogrei <vodogrei15@mail.ru>
2025-03-19 17:04:28 +0300

    remate form>add slider>add footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,14 +1,98 @@
-import { Navigation } from '../navigation/navigation';
-import css from './footer.module.scss'
+import Link from "next/link";
+import css from "./footer.module.scss";
 
 export const Footer = () => {
-    return (
-        <div className={css.footer__wrapper}>
-            <div className={css.footer__container}>
-                <div className={css.header__nav}>
-                    <Navigation/>
+  return (
+    <div className={css.footer__wrapper}>
+      <div className={css.footer__container}>
+        <section className={css.footer__nav}>
+          <ul className={css.nav__list}>
+            <li className={css.nav__item}>
+              <Link href="#" className="container__nav_link">
+                Условия
+              </Link>
+            </li>
+            <li className={css.nav__item}>
+              <Link href="#" className="container__nav_link">
+                О нас
+              </Link>
+            </li>
+            <li className={css.nav__item}>
+              <Link href="#" className="container__nav_link">
+                Базис салон
+              </Link>
+            </li>
+            <li className={css.nav__item}>
+              <Link href="#" className="container__nav_link">
+                Каталог услуг
+              </Link>
+            </li>
+            <li className={css.nav__item}>
+              <Link href="#" className="container__nav_link">
+                Шкафы-купе
+              </Link>
+            </li>
+            <li className={css.nav__item}>
+              <Link href="#" className="container__nav_link">
+                Калькулятор
+                <br />
+                витрин
+              </Link>
+            </li>
+          </ul>
+          <ul className={css.nav__list}>
+            <li className={css.nav__item}>
+              <Link href="#" className="container__nav_link">
+                Раскрой онлайн
+              </Link>
+            </li>
+            <li className={css.nav__item}>
+              <Link href="#" className="container__nav_link">
+                Доставка
+              </Link>
+            </li>
+            <li className={css.nav__item}>
+              <Link href="#" className="container__nav_link">
+                Расчет фасадов
+                <br />
+                онлайн
+              </Link>
+            </li>
+          </ul>
+        </section>
+        <section className={css.footer__contact}>
+            <div className={css.contact__map}>
+                <div style={{position:'relative', overflow:'hidden'}}>
+                    <a
+                    href="https://yandex.ru/maps/org/fdm/60072063603/?utm_medium=mapframe&utm_source=maps"
+                    style={{color:'#eee', fontSize:'12px', position:'absolute', top:'0px'}}
+                    >
+                    ФДМ
+                    </a>
+                    <a
+                    href="https://yandex.ru/maps/16/yaroslavl/category/furniture_factory/184106634/?utm_medium=mapframe&utm_source=maps"
+                    style={{color:'#eee', fontSize:'12px', position:'absolute', top:'14px'}}
+                    >
+                    Мебельная фабрика в Ярославле
+                    </a>
+                    <a
+                    href="https://yandex.ru/maps/16/yaroslavl/category/warehouse/160816688334/?utm_medium=mapframe&utm_source=maps"
+                    style={{color:'#eee', fontSize:'12px', position:'absolute', top:'28px'}}
+                    >
+                    Склад в Ярославле
+                    </a>
+                    <iframe
+                    src="https://yandex.ru/map-widget/v1/?ll=39.818495%2C57.676193&mode=poi&poi%5Bpoint%5D=39.816762%2C57.676539&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D60072063603&z=18.38"
+                    width="360"
+                    height="214"
+                    frameborder="1"
+                    allowfullscreen="true"
+                    style={{position:'relative'}}
+                    ></iframe>
                 </div>
-            </div>
-        </div>
-    );
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 };
--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,49 +1,29 @@
-'use client'
-import React, { useState, useEffect } from 'react';
-import { Button_Origin } from '../../components/ui/buttons/button/button-origin';
-import css from './form.module.scss'
+import { Button_Origin } from "../../components/ui/buttons/button/button-origin";
+import css from "./form.module.scss";
 
 export const Form = () => {
+  return (
+    <div className={css.form__container}>
+      <div className={css.form__title}>
+        <h2>Оставьте заявку на расчет</h2>
+      </div>
+      <section className={css.form__blocks}>
+        <div className={css.form__items}>
+          <form method="post">
+            <input type="text" id="name" name="name" placeholder="ИМЯ" required
+            />
 
-    const [comment, setComment] = useState('');
+            <input type="email" id="email" name="email" placeholder="E-MAIL" required
+            />
 
-    useEffect(() => {
-        // Получаем комментарий из localStorage только на стороне клиента
-        const storedComment = localStorage.getItem('commentDraft') || '';
-        setComment(storedComment);
-    }, []); // Запускаем только один раз при монтировании
-
-    const handleChange = (event) => {
-        const newComment = event.target.value;
-        setComment(newComment);
-        localStorage.setItem('commentDraft', newComment);  // Устанавливаем комментарий при изменении в useEffect, чтобы обеспечить его выполнение только на стороне клиента
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // Отправьте комментарий на сервер или выполните другие действия
-        localStorage.removeItem('commentDraft'); // Удалите черновик из localStorage
-        setComment(''); // Очистите поле
-    };
-
-    return (
-        <div className={css.form__container}>
-            <div className={css.form__title}>
-                <h2>Оставьте заявку на расчет</h2>
-            </div>
-            <div className={css.form__items}>
-                <form method='post' onSubmit={handleSubmit}>
-                    <input type="text" id="name" name="name" placeholder='ИМЯ' required/>
-
-                    <input type="email" id="email" name="email" placeholder='E-MAIL' required/>
-
-                    <input type="tel" id="phone" name="phone"placeholder='ТЕЛЕФОН' required/>
-
-                    <textarea onChange={handleChange} value={comment} id="message" name="message" spellCheck='true' placeholder='ВАШЕ СООБЩЕНИЕ'/>
-
-                    <Button_Origin/>
-                </form>
-            </div>
+            <input type="tel" id="phone" name="phone" placeholder="ТЕЛЕФОН" required
+            />
+          </form>
         </div>
-    );
+        <div className={css.form__button}>
+          <Button_Origin />
+        </div>
+      </section>
+    </div>
+  );
 };
