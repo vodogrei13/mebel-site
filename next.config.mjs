21d65cfa08932ae886754d6df558eac2359356f9
vodogrei13 <vodogrei15@mail.ru>
2025-04-29 16:34:48 +0300

    fix basePath

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,18 @@
 /** @type {import('next').NextConfig} */
 
 // Проверяем окружение: если это продакшен на GitHub, используем basePath
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 const nextConfig = {
   output: 'export',
-  basePath: isGithubActions ? '/mebel-site' : '',
-  assetPrefix: isGithubActions ? '/mebel-site/' : '',
+  basePath,
+  assetPrefix: basePath + '/',
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   poweredByHeader: false,
-    images: {
-        domains: ['localhost'],
-        unoptimized: true,
-    },
+  images: {
+    domains: ['localhost'],
+    unoptimized: true,
+  },
     // experimental: {
     //     optimizePackageImports: { 
             // *ДЛЯ ОПТИМИЗАЦИИ МОДУЛЕЙ*
